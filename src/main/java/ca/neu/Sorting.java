b763e47a0c9fd58e6c80559542a4ae60b52cdda7
zcistkidd <zcistkidd@gmail.com>
2022-02-06 20:24:16 -0800

    Used bitwise operator to deal with divide, multiply and module for 2**N

--- src/main/java/ca/neu/Sorting.java
+++ src/main/java/ca/neu/Sorting.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
+
 /**
  * Homework - Searching
  * Find the selected number in the list.
@@ -19,113 +20,115 @@ import java.util.stream.Stream;
  */
 public class Sorting {
 
-  protected List list = new ArrayList<Integer>();
-  private Random random = new Random();
+    protected List list = new ArrayList<Integer>();
+    private Random random = new Random();
 
-  /**
-   * Loading the text files with double numbers
-   */
-  protected void load() {
-    try (Stream<String> stream = Files.lines(Paths.get("numbers.txt"))) {
-      stream.forEach(x -> list.add(Integer.parseInt(x)));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * Testing of your solution, using 100 shuffled examples
-   *
-   * @return execution time
-   */
-  protected long test() {
-    Instant start = Instant.now();
-    for (int i = 0; i < 100; i++) {
-      Collections.shuffle(list, new Random(100));
-      sort(list, (Integer) list.get(random.nextInt(list.size())));
-    }
-    Instant finish = Instant.now();
-    return Duration.between(start, finish).toMillis();
-  }
-
-
-  /**
-   * Searching method - add your code in here
-   *
-   * @param list - list to be sorted and the searched
-   */
-  private void sort(List list, int index) {
-    int[] unsortedList =  new int[list.size()];
-    for (int i = 0; i < list.size(); i++)
-      unsortedList[i] = (int) list.get(i);
-
-    radixSort(unsortedList, list.size());
-//        Collections.sort(list);
-  }
-
-  /**
-   * Radix sort.
-   * Instead of using base of 10, using 256 as base can be more efficient.
-   *
-   * @param list the list
-   * @param n    size of list
-   */
-  private void radixSort(int[] list, int n) {
-    int max = maxValue(list);
-    int base = 256;
-    for (int exp = 1; max / exp > 0; exp *= base)
-      countSort(list, n, exp, base);
-  }
-
-
-  /**
-   * Counting sort.
-   *
-   * @param list unsorted list
-   * @param n    size of list
-   * @param exp
-   * @param base
-   */
-  static void countSort(int[] list, int n, int exp, int base) {
-    int sortedList[] = new int[n];
-    int count[] = new int[base];
-    int mod[] = new int[n];
-
-    // Store count of occurrences in count[]
-    for (int i = 0; i < n; i++) {
-      mod[i] = (list[i] / exp) % base;
-      count[mod[i]]++;
+    /**
+     * Loading the text files with double numbers
+     */
+    protected void load(){
+        try (Stream<String> stream = Files.lines(Paths.get("numbers.txt"))) {
+            stream.forEach(x -> list.add(Integer.parseInt(x)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    // Change count[i] so that count[i] now contains actual
-    // position of this digit in output[]
-    for (int i = 1; i < base; i++) {
-      count[i] += count[i - 1];
+    /**
+     * Testing of your solution, using 100 shuffled examples
+     * @return execution time
+     */
+    protected long test(){
+        Instant start = Instant.now();
+        for (int i=0; i<100; i++){
+            Collections.shuffle(list, new Random(100));
+            sort(list, (Integer) list.get(random.nextInt(list.size())));
+        }
+        Instant finish = Instant.now();
+        return Duration.between(start, finish).toMillis();
     }
 
-    // Build sorted array
-    for (int i = n - 1; i >= 0; i--) {
-      sortedList[count[mod[i]] - 1] = list[i];
-      count[mod[i]]--;
+    /**
+     * Searching method - add your code in here
+     * @param list - list to be sorted and the searched
+     */
+    private void sort(List list, int index) {
+        int[] unsortedList =  new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            unsortedList[i] = (int) list.get(i);
+
+        radixSort(unsortedList, list.size());
     }
 
-    for (int i = 0; i < n; i++) {
-      list[i] = sortedList[i];
+
+    /**
+     * Radix sort.
+     * Instead of using base of 10, using 256 as base can be more efficient.
+     *
+     * @param list the list
+     * @param n    size of list
+     */
+    private void radixSort(int[] list, int n) {
+        int max = maxValue(list);
+        int base = 256;
+        int bits = 1;
+        for (int exp = 1; max / exp > 0; exp = exp << 8)
+            bits = bits << 3;
+            countSort(list, n, bits, base);
     }
-  }
 
 
-  /**
-   * Helper method to get the max element in array
-   *
-   * @param list of integers
-   * @return the max value in the array of integers
-   */
-  private static int maxValue(int[] list) {
-    int max = list[0];
-    for (int element : list) {
-      max = element > max ? element : max;
+    /**
+     * Counting sort.
+     *
+     * @param list unsorted list
+     * @param n    size of list
+     * @param exp
+     * @param base
+     */
+    static void countSort(int[] list, int n, int exp, int base) {
+        int sortedList[] = new int[n];
+        int count[] = new int[base];
+        int mod[] = new int[n];
+
+        // Store count of occurrences in count[]
+        for (int i = 0; i < n; i++) {
+            mod[i] = (list[i] >> exp) & 255;
+            count[mod[i]]++;
+        }
+
+        // Change count[i] so that count[i] now contains actual
+        // position of this digit in output[]
+        for (int i = 1; i < base; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build sorted array
+        for (int i = n - 1; i >= 0; i--) {
+            sortedList[count[mod[i]] - 1] = list[i];
+            count[mod[i]]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            list[i] = sortedList[i];
+        }
     }
-    return max;
-  }
-}
+
+
+    /**
+     * Helper method to get the max element in array
+     *
+     * @param list of integers
+     * @return the max value in the array of integers
+     */
+    private static int maxValue(int[] list) {
+        int max = list[0];
+        for (int element : list) {
+            max = element > max ? element : max;
+        }
+        return max;
+    }
+
+
+
+    }
