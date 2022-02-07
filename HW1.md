7152e0900e10502157f60883eca66618c897917c
Yuhan Yue <yuhan0810@outlook.com>
2022-02-06 16:20:47 -0800

    radix sort

---
+++ HW1.md
@@ -0,0 +1,7 @@
+# HW1
+
+* **how to find the best base for radix sort:** 
+  * https://stackoverflow.com/questions/23145573/best-base-for-radix-sort
+  * https://cs.stackexchange.com/questions/108482/radix-sort-with-different-bases
+
+> Because of this is 1/2 or 1/4 of sizeof(int). With 256, you will use less additional memory, but 4 moves total. With 256*256, you will reach the result in 2 moves only (for each int), but you need significant extra memory for counters: sizeof(int) * 2 * 256*256 bytes = 512Mb
--- src/main/java/ca/neu/Sorting.java
+++ src/main/java/ca/neu/Sorting.java
@@ -19,76 +19,97 @@ import java.util.stream.Stream;
  */
 public class Sorting {
 
-    protected List list = new ArrayList<Integer>();
-    private Random random = new Random();
-    int RANGE = 9;
-    int max = 9999985;
+  protected List list = new ArrayList<Integer>();
+  private Random random = new Random();
 
-
-    /**
-     * Loading the text files with double numbers
-     */
-    protected void load(){
-        try (Stream<String> stream = Files.lines(Paths.get("numbers.txt"))) {
-            stream.forEach(x -> list.add(Integer.parseInt(x)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  /**
+   * Loading the text files with double numbers
+   */
+  protected void load() {
+    try (Stream<String> stream = Files.lines(Paths.get("numbers.txt"))) {
+      stream.forEach(x -> list.add(Integer.parseInt(x)));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * Testing of your solution, using 100 shuffled examples
-     * @return execution time
-     */
-    protected long test(){
-        Instant start = Instant.now();
-        for (int i=0; i<100; i++){
-            Collections.shuffle(list, new Random(100));
-            sort(list, (Integer) list.get(random.nextInt(list.size())));
-        }
-        Instant finish = Instant.now();
-        return Duration.between(start, finish).toMillis();
+  /**
+   * Testing of your solution, using 100 shuffled examples
+   *
+   * @return execution time
+   */
+  protected long test() {
+    Instant start = Instant.now();
+    for (int i = 0; i < 100; i++) {
+      Collections.shuffle(list, new Random(100));
+      sort(list, (Integer) list.get(random.nextInt(list.size())));
     }
+    Instant finish = Instant.now();
+    return Duration.between(start, finish).toMillis();
+  }
 
-    /**
-     * Searching method - add your code in here
-     * @param list - list to be sorted and the searched
-     */
-    private void sort(List list, int index)  {
 
-        radixsort(list,list.size());
+  /**
+   * Searching method - add your code in here
+   *
+   * @param list - list to be sorted and the searched
+   */
+  private void sort(List list, int index) {
+    radixSort(list.stream().mapToInt(i -> (int) i).toArray(), list.size());
 //        Collections.sort(list);
+  }
+
+  /**
+   * Do Counting Sort to  for digit at (exp)th place of the list in the Radix Sort.
+   *
+   * @param list unsorted list
+   * @param n    size of list
+   * @param exp  to get the current digit number
+   * @param base the size of array we need to sort
+   */
+  static void countSort(int[] list, int n, int exp, int base) {
+    int sortedList[] = new int[n];
+    int count[] = new int[base];
+    int mod[] = new int[n];
+    for (int i = 0; i < n; i++) {
+      mod[i] = (list[i] / exp) % base;
+      count[mod[i]]++;
     }
 
-    public void countsort(List<Integer> arr, int n, int d){
-        int[] output = new int[n];
-        int[] count = new int[RANGE+1];
+    for (int i = 1; i < base; i++)
+      count[i] += count[i - 1];
 
-        for(int i = 0; i < n; i++){
-            count[arr.get(i)/d % 10] += 1;
-        }
+    for (int i = n - 1; i >= 0; i--)
+        sortedList[count[mod[i]]-- - 1] = list[i];
 
-        for(int i = 1; i <= RANGE; i++){
-            count[i] += count[i-1];
-        }
+    for (int i = 0; i < n; i++)
+      list[i] = sortedList[i];
+  }
 
-        for(int i = n-1; i >= 0; i--){
-            output[count[arr.get(i)/d % 10]-1] = arr.get(i);
-            count[arr.get(i)/d % 10] -= 1;
-        }
+  /**
+   * Do Radix Sort for list of size n.
+   *
+   * @param list the list
+   * @param n    size of list
+   */
+  private void radixSort(int[] list, int n) {
+    int max = maxValue(list);
+    int base = 256;
+    for (int exp = 1; max / exp > 0; exp *= base)
+      countSort(list, n, exp, base);
+  }
 
-        for(int i = 0; i < n; i++){
-            arr.set(i,output[i]);
-        }
+  /**
+   * Helper method to get the max element in array
+   *
+   * @param list of integers
+   * @return the max value in the array of integers
+   */
+  private static int maxValue(int[] list) {
+    int max = list[0];
+    for (int element : list) {
+      max = element > max ? element : max;
     }
-
-    public void radixsort(List<Integer> arr, int n){
-
-        for(int d = 1; max/d > 0; d *= 10){
-            countsort(arr, n, d);
-        }
-    }
-
-
-
+    return max;
+  }
 }
